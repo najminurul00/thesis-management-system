2b8a71fa8b1aa0ba3b624463115e215b43d29413
najminurul00 <nurulnajmi.work@gmail.com>
2025-07-21 19:31:15 +0800

    Add .next/static/chunks/src_app_dashboard_admin-prodi_page_tsx_a993527f._.js 1753097473773

---
+++ .next/static/chunks/src_app_dashboard_admin-prodi_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_660d0a84._.js",
+  "static/chunks/node_modules_5484a3ef._.js"
+],
+    source: "dynamic"
+});
